17eae1f6975715768754089a9017d56d842c7e36
Alexander <sibirtsev.a@baikalsr.ru>
2022-04-26 16:42:59 +0300

    First init

--- Sources/TagsFlowLayout/TagsFlowLayout.swift
+++ Sources/TagsFlowLayout/TagsFlowLayout.swift
@@ -1,6 +1,131 @@
-public struct TagsFlowLayout {
-    public private(set) var text = "Hello, World!"
+// MIT License
+//
+// Copyright (c) 2022 Alexandr Sibirtsev
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
-    public init() {
+import UIKit
+
+public extension TagsFlowLayout {
+    
+    enum LayoutAlignment: Int {
+        case left
+        case center
+        case right
+    }
+}
+
+public class TagsFlowLayout: UICollectionViewFlowLayout {
+    
+    let alignment: LayoutAlignment
+    
+    //MARK: - Init Methods
+    
+    required init(alignment: LayoutAlignment = .left, minimumInteritemSpacing: CGFloat = 0, minimumLineSpacing: CGFloat = 0, sectionInset: UIEdgeInsets = .zero) {
+        self.alignment = alignment
+        super.init()
+        
+        self.minimumInteritemSpacing = minimumInteritemSpacing
+        self.minimumLineSpacing = minimumLineSpacing
+        self.sectionInset = sectionInset
+        
+        self.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+        self.sectionInsetReference = SectionInsetReference.fromLayoutMargins
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override func layoutAttributesForElements(in rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        guard let superArray = super.layoutAttributesForElements(in: rect) else {  return nil  }
+        guard let attributes = NSArray(array: superArray, copyItems: true) as? [UICollectionViewLayoutAttributes] else {  return nil  }
+        // Constants
+        let leftPadding: CGFloat = 8
+        let interItemSpacing = minimumInteritemSpacing
+        // Tracking values
+        var leftMargin: CGFloat = leftPadding // Modified to determine origin.x for each item
+        var maxY: CGFloat = -1.0 // Modified to determine origin.y for each item
+        var rowSizes: [[CGFloat]] = [] // Tracks the starting and ending x-values for the first and last item in the row
+        var currentRow: Int = 0 // Tracks the current row
+        attributes.forEach { layoutAttribute in
+            guard layoutAttribute.representedElementCategory == .cell else {
+                return
+            }
+            // Each layoutAttribute represents its own item
+            if layoutAttribute.frame.origin.y >= maxY {
+                // This layoutAttribute represents the left-most item in the row
+                leftMargin = leftPadding
+                // Register its origin.x in rowSizes for use later
+                if rowSizes.count == 0 {
+                    // Add to first row
+                    rowSizes = [[leftMargin, 0]]
+                } else {
+                    // Append a new row
+                    rowSizes.append([leftMargin, 0])
+                    currentRow += 1
+                }
+            }
+            layoutAttribute.frame.origin.x = leftMargin
+            leftMargin += layoutAttribute.frame.width + interItemSpacing
+            maxY = max(layoutAttribute.frame.maxY, maxY)
+            // Add right-most x value for last item in the row
+            rowSizes[currentRow][1] = leftMargin - interItemSpacing
+        }
+        
+        guard alignment != .left && alignment != .right else {
+            return attributes
+        }
+        
+        // At this point, all cells are left aligned
+        // Reset tracking values and add extra left padding to center align entire row
+        leftMargin = leftPadding
+        maxY = -1.0
+        currentRow = 0
+        attributes.forEach { layoutAttribute in
+            // Each layoutAttribute is its own item
+            if layoutAttribute.frame.origin.y >= maxY {
+                // This layoutAttribute represents the left-most item in the row
+                leftMargin = leftPadding
+                // Need to bump it up by an appended margin
+                let rowWidth = rowSizes[currentRow][1] - rowSizes[currentRow][0] // last.x - first.x
+                let appendedMargin = (collectionView!.frame.width - leftPadding  - rowWidth - leftPadding) / 2
+                leftMargin += appendedMargin
+                currentRow += 1
+            }
+            layoutAttribute.frame.origin.x = leftMargin
+            leftMargin += layoutAttribute.frame.width + interItemSpacing
+            maxY = max(layoutAttribute.frame.maxY, maxY)
+        }
+        
+        return attributes
+    }
+    
+    public override var flipsHorizontallyInOppositeLayoutDirection: Bool {
+        return true
+    }
+    
+    public override var developmentLayoutDirection: UIUserInterfaceLayoutDirection {
+        if alignment == .right {
+            return UIUserInterfaceLayoutDirection.rightToLeft
+        } else {
+            return UIUserInterfaceLayoutDirection.leftToRight
+        }
     }
 }
